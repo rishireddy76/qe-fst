0026f57ae757c003f2b18853c5c342f975f1d09e
Rishi Reddy <112603219+rishireddy76@users.noreply.github.com>
2025-05-21 16:52:03 +0530

    Add files via upload

---
+++ java/hello.java
@@ -0,0 +1,12 @@
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "Rishi" ;
+		System.out.println(name);
+		name = "Reddy";
+		System.out.println(name);
+	}
+
+}
